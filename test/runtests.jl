501329b76b3a233b24b9327fe0fabf2f315dc9f7
Andrew Ning <aning@byu.edu>
2019-12-05 14:34:05 -0700

    add unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,130 @@ using FLOWMath
 using Test
 
 @testset "FLOWMath.jl" begin
-    # Write your own tests here.
+
+
+# ------ trapz --------
+# tests from matlab trapz docs
+x = 1:5
+y = [1 4 9 16 25]
+z = trapz(x, y)
+@test z == 42.0
+
+x = range(0.0, pi+1e-15, step=pi/100)
+y = sin.(x)
+z = trapz(x, y)
+@test isapprox(z, 1.999835503887444, atol=1e-15)
+
+# -------------------------    
+
+# ------ Brent's method ------
+
+f(x) = x^2 - 1.0
+
+xstar = brent(f, -2.0, 0)
+@test xstar == -1.0
+xstar = brent(f, 0.0, 2)
+@test xstar == 1.0
+
+f(x) = x^3 - 1
+xstar = brent(f, 0, 3)
+@test xstar == 1.0
+
+f(x) = sin(x)
+atol = 2e-12
+xstar = brent(f, 1, 4, atol=atol)
+@test isapprox(xstar, pi, atol=atol)
+
+atol = 1e-15
+xstar = brent(f, 1, 4, atol=atol)
+@test isapprox(xstar, pi, atol=atol)
+
+# -------------------------
+
+# ------ abs_smooth ---------
+x = -3.0
+delta_x = 0.0
+y = abs_smooth(x, delta_x)
+@test y == 3.0
+
+x = 3.0
+delta_x = 0.0
+y = abs_smooth(x, 0.1)
+@test y == 3.0
+
+x = -3.0
+delta_x = 0.1
+y = abs_smooth(x, delta_x)
+@test y == 3.0
+
+x = 0
+delta_x = 0.1
+y = abs_smooth(x, delta_x)
+@test y == 0.05
+
+x = 0.1
+delta_x = 0.1
+y = abs_smooth(x, delta_x)
+@test y == 0.1
+
+x = -0.1
+delta_x = 0.1
+y = abs_smooth(x, delta_x)
+@test y == 0.1
+
+x = 0.05
+delta_x = 0.1
+y = abs_smooth(x, delta_x)
+@test y == 0.0625
+
+x = -0.05
+delta_x = 0.1
+y = abs_smooth(x, delta_x)
+@test y == 0.0625
+# -------------------------
+
+
+# ------- forward/backwards/central/complex diff ----------
+
+f(x) = @. exp(x) / sqrt(sin(x)^3 + cos(x)^3)
+
+function dfdx(x)
+    B = @. sqrt(sin(x)^3 + cos(x)^3)
+    T = @. exp(x)
+    dT = @. exp(x)
+    dB = @. 0.5/sqrt(sin(x)^3 + cos(x)^3)*3*(sin(x)^2*cos(x) - cos(x)^2*sin(x))
+    return @. (B*dT - T*dB)/B^2
+end
+
+x = [1.5, 0.5]
+dff = forwarddiff(f, x)
+dfb = backwarddiff(f, x)
+dfc = centraldiff(f, x)
+dfcplx = complexstep(f, x)
+dfexact = dfdx(x)
+@test isapprox(dff[1, 1], dfexact[1], atol=1e-4)
+@test isapprox(dfb[1, 1], dfexact[1], atol=1e-4)
+@test isapprox(dfc[1, 1], dfexact[1], atol=1e-6)
+@test isapprox(dfcplx[1, 1], dfexact[1], atol=1e-15)
+@test isapprox(dff[2, 2], dfexact[2], atol=1e-4)
+@test isapprox(dfb[2, 2], dfexact[2], atol=1e-4)
+@test isapprox(dfc[2, 2], dfexact[2], atol=1e-6)
+@test isapprox(dfcplx[2, 2], dfexact[2], atol=1e-15)
+
+
+
+# -------------------------------------------------------
+
+
+# ---------- interpolate -------------
+
+x = 0:pi/4:2*pi
+y = sin.(x)
+xpt = 0:pi/16:2*pi
+
+ypt = akima(x, y, xpt)
+
+# ---------------------------
+
+
 end
