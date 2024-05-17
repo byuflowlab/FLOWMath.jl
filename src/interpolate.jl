00cb54b890e446184925f0846a6c14ed32bc4764
Judd Mehr <juddmehr@gmail.com>
2024-05-17 15:32:20 -0600

    add default arguments to akima docstring

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -146,6 +146,11 @@ See docstring for Akima for more details.
 **Arguments**
 - `x, y::Vector{Float}`: the node points
 - `xpt::Vector{Float} or ::Float`: the evaluation point(s)
+- `delta_x::Float=0.0` : the half width of a smoothing interval used for the absolute
+value function.
+- `eps::Float=1e-30` : a cutoff used to avoid dividing by zero in the 
+weighting function. Default is `1e-30` but this could be raised to machine precision in 
+some cases to improve derivatives.
 
 **Returns**
 - `ypt::Vector{Float} or ::Float`: interpolated value(s) at xpt using akima spline.
